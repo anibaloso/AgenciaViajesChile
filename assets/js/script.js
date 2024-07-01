d6213c2de7f666436692f39a957e2722b12f7e85
anibal <anibaloso@live.com>
2024-07-01 13:20:54 -0400

    Se crea nueva rama, agrega funcionalidad

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$('#enviarCorreo').click(function(){
+    alert('Mensaje enviado con exito')
+})
+
